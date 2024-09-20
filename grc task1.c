19d0a3228e266e256162d28e889ca00016a86154
Ankita-b-prog <at9855299@gmail.com>
2024-09-20 09:49:03 +0530

    Add files via upload

---
+++ grc task1.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int x;
+      printf("enter the value:");
+      scanf("%d",&x);
+  if(x%2==0){
+  printf("%d is even",x);
+  }else{
+  printf("%d is odd",x);
+  }
+}  
+  
+   
