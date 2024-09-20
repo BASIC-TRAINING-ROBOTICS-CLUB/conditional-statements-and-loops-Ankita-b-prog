114d09282b695421ee07f4ebf0ae359bd58d87f3
Ankita-b-prog <at9855299@gmail.com>
2024-09-20 09:53:10 +0530

    Add files via upload

---
+++ grc task6.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+void main(){
+    int a;
+      printf("enter the value:");
+      scanf("%d",&a);
+  if(a==0){
+      printf("a is zero");
+  }else if(a<0){
+      printf("%d is negative number",a);
+  } else{
+  printf("%d is positive number",a);
+  }
+  
+}  
+  
+   
