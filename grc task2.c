c2d22c3ff86bb42cdb6e0b16dbac6c7b98e883ad
Ankita-b-prog <at9855299@gmail.com>
2024-09-20 09:50:32 +0530

    Add files via upload

---
+++ grc task2.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+void main(){
+    int x,y,z;
+      printf("enter the value:x,y,z:");
+      scanf("%d %d %d",&x,&y,&z);
+  if(x==y && y==z){
+  printf("equilateral triangle");
+  }else{
+  printf("not equilateral tringle");
+  }
+}  
+  
+   
